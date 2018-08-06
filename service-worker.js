6c8abb48f5112dc789bc2c6b4985767080fcbe68
Maxime Blanc <maxime.blanc@m6.fr>
2018-08-06 14:18:55 +0200

    add service worker

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js"
+);
+
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
+
+const cacheExpiration = new workbox.expiration.Plugin({
+  maxAgeSeconds: 7 * 24 * 60 * 60
+});
+
+workbox.routing.registerRoute(
+  /https:\/\/api.jikan.moe\/.*/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "api",
+    plugins: [cacheExpiration]
+  })
+);
+
+workbox.routing.registerRoute(
+  /https:\/\/myanimelist.cdn-dena.com\/r\/100x140\/images.*/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: "images",
+    plugins: [cacheExpiration]
+  })
+);
--- src/index.js
+++ src/index.js
@@ -15,3 +15,19 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("app")
 );
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", async () => {
+    try {
+      const registration = await navigator.serviceWorker.register(
+        "../service-worker.js"
+      );
+      console.log(
+        "ServiceWorker registration successful with scope: ",
+        registration.scope
+      );
+    } catch (error) {
+      console.log("ServiceWorker registration failed: ", error);
+    }
+  });
+}
