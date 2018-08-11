892a05c4cab88ea34ad1ae08c69f146697f85047
maxime blanc <maximeblanc.dev@gmail.com>
2018-08-11 07:43:40 +0200

    model tests

---
+++ src/models/__tests__/App.spec.js
@@ -0,0 +1,23 @@
+import { Manga } from "../Manga";
+
+describe("<=== Manga ===>", () => {
+  let manga = null;
+
+  beforeEach(() => {
+    manga = Manga.create({
+      id: 43,
+      title: "One Piece",
+      imageUrl: "http://one-piece.jpg",
+      startDate: "13 aout 2016",
+      endDate: null,
+      url: "http://one-piece.fr",
+      rank: 1,
+      score: 9.12,
+      type: "shonen"
+    });
+  });
+
+  it("should create store", () => {
+    expect(manga).toMatchSnapshot();
+  });
+});
